2cf3cf0b82382a572db205aa644f47dc14e70664
mamanimartinpablo <mamanimartinpablo@gmail.com>
2024-07-11 17:32:24 -0300

    logout condicional

--- js/tpHeader.js
+++ js/tpHeader.js
@@ -8,7 +8,7 @@ document.head.appendChild(link);
 
 // Escribir el contenido HTML después de cargar los estilos de FontAwesome
 document.write('<h1><i class="fas fa-car"></i>&nbsp;&nbsp;&nbsp;BLOG DE AUTOS ANTIGUOS</h1>');
-document.write('<nav> <input type="checkbox" id="check"> </input> <label for="check" class="checkbtn"> <i class="fas fa-bars"></i> </label> <ul> <li><a class="active" href="index.html">Home</a></li>    <li><a href="mas_buscados.html">Más buscados</a></li> <li class="nav-item dropdown" id="crud"   ><a class="dropdown-item" href="informacion.html">CRUD autos</a></li><li class="nav-item"><a class="nav-link" href="registro.html">Registro</a></li><li class="nav-item"><a class="nav-link" href="login.html">Login</a></li> <button onclick="logout()" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-log-out"></span>Logout</button>  </ul>    </nav>  ')
+document.write('<nav> <input type="checkbox" id="check"> </input> <label for="check" class="checkbtn"> <i class="fas fa-bars"></i> </label> <ul> <li><a class="active" href="index.html">Home</a></li>    <li><a href="mas_buscados.html">Más buscados</a></li> <li class="nav-item dropdown" id="crudv"   ><a class="dropdown-item" href="informacion.html">CRUD autos</a></li><li class="nav-item"><a class="nav-link" href="registro.html">Registro</a></li><li class="nav-item"><a class="nav-link" href="login.html">Login</a></li> <button  id="logout" onclick="logout()" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-log-out"></span>Logout</button>  </ul>    </nav>  ')
 // document.write
 // ('<nav><a href="index.html">Home</a>  <a href="informacion.html">Información de Autos</a> <a href="mas_buscados.html">Los Autos más buscados</a> <a href="registro.html">Registrate</a></nav>');
 
@@ -20,7 +20,14 @@ document.write('<nav> <input type="checkbox" id="check"> </input> <label for="ch
 
 
 if (sessionStorage.getItem("adm")!="1"){
-    document.querySelector("#crud").setAttribute('style', 'display:none')
+    document.querySelector("#crudv").setAttribute('style', 'display:none')
 }else{
-    document.querySelector("#crud").setAttribute('style', 'display:on')
+    document.querySelector("#crudv").setAttribute('style', 'display:on')
+}
+
+
+if(sessionStorage.getItem("adm")==null){
+    document.querySelector("#logout").setAttribute('style', 'display:none')
+}else{
+    document.querySelector("#logout").setAttribute('style', 'display:on')
 }
